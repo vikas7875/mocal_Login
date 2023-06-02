edc9e7b8706239be0bd5bf9f55c1a87a67bd6574
abc <abc@gmail.com>
2023-06-03 02:12:26 +0530

    background added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'loginForm';
+  template = `We'll help you find the perfect timewith your <br>
+  clients. Please enter your details to start with <br>
+   and we'll fit you right in.`
 }
